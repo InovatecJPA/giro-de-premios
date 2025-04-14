3143614ad28dbb5ed82b406b268aad6d17f9e6fd
pbravim <bravim.pedro@gmail.com>
2025-04-14 15:47:04 -0300

    add prisma as ORM for project and schema implementation

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
-export default class PrismaModule {}
+export class PrismaModule {}
