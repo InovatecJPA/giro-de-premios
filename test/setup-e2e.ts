d35e56200d76a3cb6b32776d00587cf3ff610db7
pbravim <bravim.pedro@gmail.com>
2025-04-16 15:02:39 -0300

    config dos testes

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,9 +1,14 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import { execSync } from 'node:child_process';
 import { randomUUID } from 'node:crypto';
+import { AppModule } from '../src/app.module';
+import { Test } from '@nestjs/testing';
 
 const prisma = new PrismaClient()
+export let schemaId: string
+export let app: INestApplication
 
 function generateDatabaseUrl(schemaId: string) {
     if (!process.env.DATABASE_URL) {
@@ -13,28 +18,53 @@ function generateDatabaseUrl(schemaId: string) {
     const url = new URL(process.env.DATABASE_URL)
 
     url.searchParams.set('schema', schemaId)
-
     return url.toString()
 }
 
-const schemaId = randomUUID()
 
-beforeAll(async () => {
+beforeEach(async () => {
+    await defineDatabaseSchema()
+    await initializeApp()
+});
+
+
+
+afterEach(async () => {
+    await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+})
+
+afterAll(async () => {
+    await prisma.$disconnect()
+})
+
+async function initializeApp() {
+    const module = await Test.createTestingModule({
+        imports: [AppModule],
+    })
+        .compile();
+
+    app = module.createNestApplication();
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true
+        })
+    )
+
+    await app.init()
+}
+
+async function defineDatabaseSchema() {
+    schemaId = randomUUID()
     process.env.DATABASE_URL = generateDatabaseUrl(schemaId)
 
-
     await prisma.$executeRawUnsafe(`CREATE SCHEMA IF NOT EXISTS "${schemaId}"`);
-  
+
     execSync('npx prisma migrate deploy', {
         env: {
             ...process.env,
         }
     });
-});
-    
-
-
-afterAll(async () => {
-    await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
-    await prisma.$disconnect()
-})
+}
---
+++ test/utils/user-test-helper.ts
@@ -0,0 +1,33 @@
+import { Profiles } from "@prisma/client";
+
+export type UserTest = {
+    name: string,
+    email: string,
+    cpf: string,
+    number: string,
+    profile: Profiles,
+    comissao?: string | null,
+    password: string,
+    social_media?: string | null
+    owner_id?: string | null
+}
+
+export const defaultUser: UserTest = {
+    name: "Default User",
+    email: "user@example.com",
+    cpf: "11111111111",
+    number: "83991758296",
+    profile: Profiles.influencer,
+    comissao: "0.10",
+    password: "Inova@123",
+}
+
+export const defaultOwner: UserTest = {
+    name: "Default owner",
+    email: "owner@example.com",
+    cpf: "99999999999",
+    number: "83991758296",
+    profile: Profiles.owner,
+    social_media: null,
+    password: "Inova@123"
+}
