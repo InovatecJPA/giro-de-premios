33e967e8e16a6d67f9154582f1d9aaf471c1c31e
pbravim <bravim.pedro@gmail.com>
2025-04-28 14:42:14 -0300

    ticket payment

--- src/raffle/ticket-payment/ticket-payment.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { TicketPaymentService } from "./ticket-payment.service";
-
-@Controller('ticket-payment')
-export class TicketPaymentController {
-    constructor(
-        private readonly ticketPaymentService: TicketPaymentService
-    ) { }
-}
